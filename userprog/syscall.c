177633e09425268eefbf65fd57feefc7817d2ac6
Ahmed Miljau <ahmed.miljau@gmail.com>
2019-09-08 01:15:49 +0500

    Validate user pointers

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -22,6 +22,9 @@ void sys_exit(int status);
 // Helpers
 // Loads a paramters from a interrupt frame's stack.
 static uint32_t load_param(struct intr_frame *frame, int offset);
+static int get_user(const uint8_t *uaddr);
+
+static bool put_user(uint8_t *udst, uint8_t byte);
 
 void syscall_init(void) {
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -39,6 +42,9 @@ static void syscall_handler(struct intr_frame *frame UNUSED) {
 }
 
 static uint32_t load_param(struct intr_frame *frame, int offset) {
+  if (get_user(frame->esp + offset) == -1) {
+    sys_exit(-1);
+  }
   return *((uint32_t *)frame->esp + offset);
 }
 
@@ -47,4 +53,27 @@ void sys_exit(int status) {
   struct thread *curr = thread_current();
   curr->exit_code = status;
   thread_exit();
+}
+
+// Lifted from https://web.stanford.edu/~ouster/cgi-bin/cs140-spring18/pintos/pintos_3.html#SEC32
+/* Reads a byte at user virtual address UADDR.
+   UADDR must be below PHYS_BASE.
+   Returns the byte value if successful, -1 if a segfault
+   occurred. */
+static int get_user(const uint8_t *uaddr) {
+  int result;
+  asm("movl $1f, %0; movzbl %1, %0; 1:" : "=&a"(result) : "m"(*uaddr));
+  return result;
+}
+
+// Lifted from https://web.stanford.edu/~ouster/cgi-bin/cs140-spring18/pintos/pintos_3.html#SEC32
+/* Writes BYTE to user address UDST.
+   UDST must be below PHYS_BASE.
+   Returns true if successful, false if a segfault occurred. */
+static bool put_user(uint8_t *udst, uint8_t byte) {
+  int error_code;
+  asm("movl $1f, %0; movb %b2, %1; 1:"
+      : "=&a"(error_code), "=m"(*udst)
+      : "q"(byte));
+  return error_code != -1;
 }
